211452b37eedebe16eb770bca7c7f4aec0efc081
Ken <fieldken@hotmail.com>
2022-05-18 14:03:37 +0200

    Vanaf nu is optellen een extension method

--- GitBasics/Program.cs
+++ GitBasics/Program.cs
@@ -1,6 +1,9 @@
 ﻿Console.WriteLine("Eerste git push!");
 
-static int Optellen(int getal1, int getal2)
-{
-    return getal1 + getal2;
+public static class Extensions
+{   
+    public static void Optellen(ref this int getal1, int getal2)
+    {
+        getal1 += getal2;
+    }
 }
